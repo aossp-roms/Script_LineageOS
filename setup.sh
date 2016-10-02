b2fe5311d1b097c9dbc76f2f79f1b6198763df6d
Sergio <sergiohernandez2b@gmail.com>
2016-10-02 19:20:52 +0200

    First commit

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Specify colors utilized in the terminal
+normal='tput sgr0'              # White
+red='tput setaf 1'              # Red
+green='tput setaf 2'            # Green
+yellow='tput setaf 3'           # Yellow
+blue='tput setaf 4'             # Blue
+violet='tput setaf 5'           # Violet
+cyan='tput setaf 6'             # Cyan
+white='tput setaf 7'            # White
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) # Bold Red
+bldgrn=${txtbld}$(tput setaf 2) # Bold Green
+bldblu=${txtbld}$(tput setaf 4) # Bold Blue
+bldylw=$(txtbld)$(tput setaf 3) # Bold Yellow
+bldvlt=${txtbld}$(tput setaf 5) # Bold Violet
+bldcya=${txtbld}$(tput setaf 6) # Bold Cyan
+bldwht=${txtbld}$(tput setaf 7) # Bold White
+
+clear
+
+echo -e "${bldred}          ___          ___     "
+        echo -e "${bldred}                               "
+        echo -e "${bldred}       CyanogenMod Installer   "
+        echo -e "${bldred}                               "
+        echo -e "${bldred}     Installing dependencies   "
+        echo -e "${bldred}                               "
+        echo -e "${bldcya}     Setting up your computer! "
