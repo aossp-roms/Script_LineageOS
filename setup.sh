877aede2e8fb90f64e3549535e088d09ecdbe794
Pablo <trujulu@gmail.com>
2016-10-18 21:56:39 +0200

    Add cmake to dependencies

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ tput setaf 2
 	time sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk3.0-dev \
 squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush \
 schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib lib32z1-dev lib32ncurses5-dev \
-lib32readline6-dev gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc patch libtool \
+lib32readline6-dev gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc patch cmake libtool \
 automake g++ gawk subversion expat libexpat1-dev python-all-dev libgcc1:i386 bc libcloog-isl-dev \
 libcap-dev autoconf libgmp-dev pkg-config libmpc-dev libmpfr-dev lzma* \
 liblzma* w3m phablet-tools android-tools-adb screen maven tmux
